8796e939bed6888e37d5816052f85805337b45d1
zhou <zhou@house.(none)>
2013-03-25 09:19:03 +0800

    first commit

---
+++ second_step/expect_change_spec.rb
@@ -0,0 +1,23 @@
+class Counter
+  class << self
+    def increment
+      @count ||= 0
+      @count += 1
+    end
+    
+    def count
+      @count ||= 0
+    end
+  end
+end
+
+describe Counter, "#increment" do
+  it "should increment the count" do
+    expect{Counter.increment}.to change{Counter.count}.from(0).to(1)
+  end
+
+  # deliberate failure
+  it "should increment the count by 2" do
+    expect{Counter.increment}.to change{Counter.count}.by(2)
+  end
+end
