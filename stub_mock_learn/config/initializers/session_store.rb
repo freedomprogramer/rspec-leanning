b1d418affbb7f6825774e713529fba7962bf9550
Jackie Lee <freedomprogramer@gmail.com>
2013-03-31 23:07:42 +0800

    merge

--- stub_mock_learn/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-StubMockLearn::Application.config.session_store :cookie_store, key: '_stub_mock_learn_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# StubMockLearn::Application.config.session_store :active_record_store
--- stub_mock_learn/spec/views/messages/show.html.erb_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe "messages/show.html.erb" do
-  it "displays the text attribute of the message" do
-    render
-    rendered.should contain("Hello world!")
-  end
-end
